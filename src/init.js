8d585fca1595db6aa70b7b8c3c44710171e2e1dc
jsmithb117 <jsmithb117@gmail.com>
2020-11-06 19:04:32 -0800

    Complete dancer interactions

--- src/init.js
+++ src/init.js
@@ -12,43 +12,92 @@ $(document).ready(function() {
       $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    dancer.distances ={};
+    dancer.index = window.dancers.length;
+    dancer.top = dancer['$node'][0].style.top;
+    dancer.left = dancer['$node'][0].style.left;
     var distanceTo = function (topCur, topTo, leftCur, leftTo) {
-      topCur = Number(topCur.toString().slice(0, topCur.length-2));
-      topTo = Number(topTo.toString().slice(0, topTo.length-2));
-      leftCur = Number(leftCur.toString().slice(0, leftCur.length-2));
-      leftTo = Number(leftTo.toString().slice(0, leftTo.length-2));
       var a = Math.abs(topCur - topTo);
       var b = Math.abs(leftCur - leftTo);
-      // return Math.sqrt((a ** 2) + (b ** 2));
+      return Math.sqrt((Math.pow(a, 2)) + (Math.pow(b, 2)));
     };
+    for (let i = 0; i < window.dancers.length; i++ ){
+      var topCur = Number(dancer['$node'][0].style.top.slice(0, -2));
+      var leftCur = Number(dancer['$node'][0].style.left.slice(0, -2));
+      var topTo = Number(window.dancers[i]['$node'][0].style.top.slice(0, -2));
+      var leftTo = Number(window.dancers[i]['$node'][0].style.left.slice(0, -2));
+      var distance = distanceTo(topCur, topTo, leftCur, leftTo);
+      dancer.distances[i] = distance;
+      window.dancers[i]['distances'][dancer.index] = distance;
+    }
 
     $('body').append(dancer.$node[0]);
-    window.dancers.push(dancer.$node[0]);
-    for (let i = 0; i < window.dancers.length; i++ ){
-      var topCur = window.dancers[window.dancers.length - 1].style.top;
-      var leftCur = window.dancers[window.dancers.length - 1].style.left;
-
-      var topTo = window.dancers[i].style.top;
-
-      var leftTo = window.dancers[i].style.left;
-
-      var distance = distanceTo(topCur, topTo, leftCur, leftTo);
-    }
+    window.dancers.push(dancer);
   });
 
   $('.lineUp').click(function() {
     for (var i = 0; i < window.dancers.length; i++) {
-      originalTops.push(window.dancers[i].style.top);
-      window.dancers[i].style.top = '50%';
+      window.dancers[i]['$node'][0].style.top = '50%';
     }
   });
 
   $('.backToOG').click(function() {
     for (let i = 0; i < window.dancers.length; i++) {
-      window.dancers[i].style.top = originalTops[i];
+      window.dancers[i]['$node'][0].style.top = window.dancers[i].top;
     }
   });
 
+  $('.pythagGroup').click(function() {
+    var classes = ['wicked', 'weird'];
 
+    for (var i = 0; i < window.dancers.length; i++) {
+      var currentNode = window.dancers[i];
+      var currentNodeDistances = currentNode.distances;
+      var randomClass = classes[Math.floor(Math.random() * classes.length)];
+      if (!currentNode.modified) {
+        currentNode['$node'][0].classList.value = randomClass;
+      }
+      for (var node in currentNodeDistances) {
+        var distance = currentNodeDistances[node];
+        if (distance <= 200) {
+          if (!window.dancers[Number(node)].modified) {
+            window.dancers[Number(node)]['$node'][0].classList.value = randomClass;
+            window.dancers[Number(node)].modified = true;
+          }
+        }
+      }
+    }
+  });
 });
 
+//uncomment line 22 and use Math.pow so pomander doesn't complain.
+
+//move window.dancers iteration before push and append
+//add 'distances' property of dancer
+//set 'index' property of dancer to window.dancers.length
+
+//currently, the push command pushes only the node, change it to add the entire dancer object
+//within iteration, set distance property to current node
+//within iteration, set distance property in node being iterated
+//adjust references when creating topCur, leftCur, topTo, leftTo variables to reference the new data structure in the window.dancers array
+
+//set top and left properties of each node just after it's created
+//adjust lineUp method to reference new data structure
+//adjust backToOG method to reference new data structure
+
+
+// {
+//   "$node":{
+//     "0":{},
+//     "length":1
+//   },
+//   "distances":{
+//     "0":227.46568358302338,
+//     "1":149.7277884195182,
+//     "3":565.9814798736794
+//   },
+//     "index":2,
+//     "top":"443.001px",
+//     "left":"235.046px",
+//     "modified": false
+// }
